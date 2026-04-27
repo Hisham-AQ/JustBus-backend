10e41a87f883697f35cc51bba43458411143dbe0
samer-mashni <mashnisamir@gmail.com>
2026-04-27 13:12:04 +0300

    update db config for railway

--- config/db.js
+++ config/db.js
@@ -1,17 +1,5 @@
 const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  connectTimeout: 20000, // 20 seconds
-});
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
 module.exports = pool;
-
