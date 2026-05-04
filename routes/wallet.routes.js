03a81597caa54f0596e85cfd701fc847cfb7c85c
Hisham-AQ <hisham.abuqurs@hotmail.com>
2026-05-04 21:05:32 +0300

    fix authMiddleware

--- routes/wallet.routes.js
+++ routes/wallet.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const controller = require("../controllers/wallet.controller");
 const authenticateToken = require("../middleware/authMiddleware");
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.get("/", authenticateToken, controller.getBalance);
 router.post("/topup", authenticateToken, controller.topUp);
