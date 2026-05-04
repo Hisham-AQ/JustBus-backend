f12d56c7b6d7579d21799fd15620045cb2696595
Hisham-AQ <hisham.abuqurs@hotmail.com>
2026-05-04 21:19:03 +0300

    walletController fix

--- routes/wallet.routes.js
+++ routes/wallet.routes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const controller = require("../controllers/wallet.controller");
-const authenticateToken = require("../middleware/authMiddleware");
+const walletController = require("../controllers/walletController");
 const authMiddleware = require("../middleware/authMiddleware");
 
-router.get("/", authenticateToken, controller.getBalance);
-router.post("/topup", authenticateToken, controller.topUp);
+
+router.get("/", authMiddleware, walletController.getBalance);
+router.post("/topup", authMiddleware, walletController.topUp);
 router.post("/pay", authMiddleware, walletController.payWithWallet);
 router.get("/transactions", authMiddleware, walletController.getTransactions);
 
