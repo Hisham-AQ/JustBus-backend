7cc2c4808f47981e37ffc717a2d196c263b123c8
Hisham-AQ <hisham.abuqurs@hotmail.com>
2026-05-07 16:10:17 +0300

    forget controller

--- routes/trips.routes.js
+++ routes/trips.routes.js
@@ -3,14 +3,13 @@ const router = express.Router();
 
 const tripsController = require("../controllers/trips.controller");
 const authenticateToken = require("../middleware/authMiddleware");
+const controller = require("../controllers/trips.controller");
+
 
 router.get("/cities", tripsController.getCities);
 router.get("/trips", tripsController.searchTrips);
 router.get("/my", authenticateToken, tripsController.getMyTrips);
 router.get("/trips/:tripId/seats", tripsController.getSeats);
-router.get(
-    "/live-location/:tripId",
-    controller.getLiveLocation
-);
+router.get("/live-location/:tripId",controller.getLiveLocation);
 
 module.exports = router;
