2c7f1b8349450c81c76f9154b802f1469850d4b3
Hisham-AQ <hisham.abuqurs@hotmail.com>
2026-05-11 02:59:06 +0300

    fix

--- controllers/ai.controller.js
+++ controllers/ai.controller.js
@@ -38,11 +38,10 @@ User:
 ${message}
 
 `;
-
     const response =
       await axios.post(
 
-        `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
 
         {
           contents: [
