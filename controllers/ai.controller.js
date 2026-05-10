0537d2c616ea54342fd53b16a240bcb31f84889b
Hisham-AQ <hisham.abuqurs@hotmail.com>
2026-05-11 02:47:49 +0300

    fix AI 2

--- controllers/ai.controller.js
+++ controllers/ai.controller.js
@@ -28,7 +28,7 @@ exports.chat = async (req, res) => {
 
     const model =
       genAI.getGenerativeModel({
-        model: "gemini-1.5-flash",
+       model: "gemini-pro",
       });
 
     const prompt = `
