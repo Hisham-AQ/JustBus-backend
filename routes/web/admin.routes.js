64d4c8eb13eae7d327c759862f89972bd3acf8ae
Hisham-AQ <hisham.abuqurs@hotmail.com>
2026-05-16 14:42:07 +0300

    fix

--- routes/web/admin.routes.js
+++ routes/web/admin.routes.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const authenticateToken = require("../middleware/authMiddleware");
-const allowRoles = require("../middleware/roleMiddleware");
-const usersController = require("../controllers/mobile/users.controller");
+const authenticateToken = require("../../middleware/authMiddleware");
+const allowRoles = require("../../middleware/authMiddleware");
+
+const usersController = require("../../controllers/web/users.controller");
 
 // GET users
-router.get("/users", authenticateToken, allowRoles("admin"), usersController.getUsers);
+router.get(
+  "/users",
+  authenticateToken,
+  allowRoles("admin"),
+  usersController.getUsers
+);
 
 module.exports = router;
