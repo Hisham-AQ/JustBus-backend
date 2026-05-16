6c42b2ded2f19549287ebabc95aade74304dfad3
Hisham-AQ <hisham.abuqurs@hotmail.com>
2026-05-16 14:59:53 +0300

    fdfd

--- routes/mobile/parcels.routes.js
+++ routes/mobile/parcels.routes.js
@@ -1,16 +1,31 @@
 const express = require("express");
 const router = express.Router();
 
-const authenticateToken = require("../middleware/authMiddleware");
-const parcelsController = require("../controllers/parcels.controller");
+const authenticateToken = require("../../middleware/authMiddleware");
+
+const parcelsController = require("../../controllers/mobile/parcels.controller");
+
 console.log("Controller:", parcelsController);
+
 // ================= GET PARCELS =================
-router.get("/parcels", authenticateToken, parcelsController.getParcels);
+router.get(
+  "/parcels",
+  authenticateToken,
+  parcelsController.getParcels
+);
 
 // ================= CREATE PARCEL =================
-router.post("/parcels", authenticateToken, parcelsController.createParcel);
+router.post(
+  "/parcels",
+  authenticateToken,
+  parcelsController.createParcel
+);
 
 // ================= UPDATE STATUS =================
-router.patch("/parcels/:id/status", authenticateToken, parcelsController.updateParcelStatus);
+router.patch(
+  "/parcels/:id/status",
+  authenticateToken,
+  parcelsController.updateParcelStatus
+);
 
 module.exports = router;
--- routes/mobile/userTrips.routes.js
+++ routes/mobile/userTrips.routes.js
@@ -2,15 +2,28 @@ const express = require("express");
 const router = express.Router();
 
 const tripsController = require("../../controllers/mobile/userTrips.controller");
-const authenticateToken = require("../../middleware/authMiddleware");
-const controller = require("../../controllers/mobile/userTrips.controller");
-const authMiddleware = require("../../middleware/authMiddleware");
 
+const authenticateToken = require("../../middleware/authMiddleware");
 
 router.get("/cities", tripsController.getCities);
+
 router.get("/trips", tripsController.searchTrips);
-router.get("/my", authenticateToken, tripsController.getMyTrips);
-router.get("/trips/:tripId/seats", tripsController.getSeats);
-router.get("/live-location/:tripId", authMiddleware, tripsController.getLiveLocation);
+
+router.get(
+  "/my",
+  authenticateToken,
+  tripsController.getMyTrips
+);
+
+router.get(
+  "/trips/:tripId/seats",
+  tripsController.getSeats
+);
+
+router.get(
+  "/live-location/:tripId",
+  authenticateToken,
+  tripsController.getLiveLocation
+);
 
 module.exports = router;
