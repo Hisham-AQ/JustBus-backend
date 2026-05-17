af40abdd4f6b5c064bc570fab826e7b271d6e2a0
Hisham-AQ <hisham.abuqurs@hotmail.com>
2026-05-18 02:22:50 +0300

    Last 18-5

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 app.use(cors({
   //origin: ["http://127.0.0.1:5174", "http://localhost:5174"],
-  origin: "*",
+  origin: "*",   //18-5-2026
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
